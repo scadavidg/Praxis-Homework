186e0e101d2ff3ccf7e301aa836bf4aa3dbcd1a3
Jerson Mendoza <jersondavidma@gmail.com>
2018-09-02 12:46:02 -0500

    configure database mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'; 
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://praxis:praxis1@ds239682.mlab.com:39682/praxis-homework')],
   controllers: [AppController],
   providers: [AppService],
 })
